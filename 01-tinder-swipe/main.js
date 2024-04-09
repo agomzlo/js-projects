e5dcd8a2933c7d02fdb586b4570aa795d3378d8d
agomzlo <agomzlo11@gmail.com>
2024-04-09 14:37:26 +0200

    Format documents

--- 01-tinder-swipe/main.js
+++ 01-tinder-swipe/main.js
@@ -1,13 +1,13 @@
-import persons from './people.json';
+import persons from "./people.json";
 
 const DECISION_THRESHOLD = 120;
-const $cards = document.getElementById('cards');
+const $cards = document.getElementById("cards");
 let isAnimating = false;
 let deltaX = 0;
 
-persons.forEach(person => {
-  const card = document.createElement('article');
-  card.classList.add('card');
+persons.forEach((person) => {
+  const card = document.createElement("article");
+  card.classList.add("card");
   card.innerHTML = `
     <img src="${person.image}" alt="${person.description}">
     <h2>${person.name} <span>${person.age}</span></h2>
@@ -17,28 +17,28 @@ persons.forEach(person => {
   $cards.appendChild(card);
 });
 
-function startDrag(event){
+function startDrag(event) {
   if (isAnimating) return;
 
   // Get the card that was clicked
-  const actualCard = event.target.closest('article');
+  const actualCard = event.target.closest("article");
   if (!actualCard) return;
 
   //Get initial position of mouse or finger
   const startX = event.pageX || event.touches[0].pageX;
 
   //Listen for mousemove or touchmove
-  document.addEventListener('mousemove', onMove);
-  document.addEventListener('touchmove', onMove, {passive: true});
+  document.addEventListener("mousemove", onMove);
+  document.addEventListener("touchmove", onMove, { passive: true });
 
   //Listen for mouseup or touchend
-  document.addEventListener('mouseup', onEnd);
-  document.addEventListener('touchend', onEnd);
+  document.addEventListener("mouseup", onEnd);
+  document.addEventListener("touchend", onEnd);
 
-  function onMove(event){
+  function onMove(event) {
     //Get current position of mouse or finger
     const currentX = event.pageX || event.touches[0].pageX;
-  
+
     //Calculate the distance moved
     deltaX = currentX - startX;
 
@@ -53,57 +53,62 @@ function startDrag(event){
     // if (!actualCard) return;
 
     actualCard.style.transform = `translateX(${deltaX}px) rotate(${cardDeg}deg)`;
-    actualCard.style.cursor = 'grabbing';
+    actualCard.style.cursor = "grabbing";
 
     //Change the opacity of the choice
     const opacity = Math.abs(deltaX) / 100;
     const goRight = deltaX >= 0;
 
     const choice = goRight
-      ? actualCard.querySelector('.choice.like')
-      : actualCard.querySelector('.choice.nope');
+      ? actualCard.querySelector(".choice.like")
+      : actualCard.querySelector(".choice.nope");
 
     choice.style.opacity = opacity;
   }
-  
-  function onEnd(event){
+
+  function onEnd(event) {
     //Remove the event listeners
-    document.removeEventListener('mousemove', onMove);
-    document.removeEventListener('touchmove', onMove);
-    document.removeEventListener('mouseup', onEnd);
-    document.removeEventListener('touchend', onEnd);
+    document.removeEventListener("mousemove", onMove);
+    document.removeEventListener("touchmove", onMove);
+    document.removeEventListener("mouseup", onEnd);
+    document.removeEventListener("touchend", onEnd);
 
     //Know the user decision
     const userDecision = Math.abs(deltaX) >= DECISION_THRESHOLD;
 
     // if (!actualCard) return;
 
-    if (userDecision){
+    if (userDecision) {
       const goRight = deltaX >= DECISION_THRESHOLD;
 
-      actualCard.classList.add(goRight ? 'go-right' : 'go-left');
-      actualCard.addEventListener('transitionend', () => {
-        actualCard.remove();
-      }, {once: true});
-    }else{
-      actualCard.classList.add('reset');
-      actualCard.classList.remove('go-right', 'go-left');
+      actualCard.classList.add(goRight ? "go-right" : "go-left");
+      actualCard.addEventListener(
+        "transitionend",
+        () => {
+          actualCard.remove();
+        },
+        { once: true }
+      );
+    } else {
+      actualCard.classList.add("reset");
+      actualCard.classList.remove("go-right", "go-left");
     }
 
     //Reset the variables
-    actualCard.addEventListener('transitionend', () => {
-      actualCard.removeAttribute('style');
-      actualCard.classList.remove('reset');
+    actualCard.addEventListener("transitionend", () => {
+      actualCard.removeAttribute("style");
+      actualCard.classList.remove("reset");
 
       deltaX = 0;
       isAnimating = false;
-    })
+    });
 
     //Reset the choise opacity
-    actualCard.querySelectorAll('.choice')
-    .forEach(choice => choice.style.opacity = 0);
+    actualCard
+      .querySelectorAll(".choice")
+      .forEach((choice) => (choice.style.opacity = 0));
   }
 }
 
-document.addEventListener('mousedown', startDrag);
-document.addEventListener('touchstart', startDrag, {passive: true});
+document.addEventListener("mousedown", startDrag);
+document.addEventListener("touchstart", startDrag, { passive: true });
