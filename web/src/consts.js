6a2dce09fd8006105b562954dcc61e3dc1c518d4
agomzlo <agomzlo11@gmail.com>
2024-04-10 21:01:41 +0200

    Upload project 03 to the webpage

--- web/src/consts.js
+++ web/src/consts.js
@@ -1,35 +1,49 @@
 export const PROJECTS = [
-    {
-        id: 1,
-        slug: '01-tinder-swipe',
-        title: 'Tinder Swipe',
-        description: 'Learn how to create a swipe card effect of tinder with HTML, CSS and JavaScript',
-        learnings: [
-            'Dom',
-            'Animations gestures',
-            'Mouse and finger touch events',
-            'CSS transitions'
-        ],
-        links: {
-            cover: '/projects/Project-01.webp',
-            github: 'https://github.com/agomzlo/js-projects/tree/main/01-tinder-swipe',
-            demo: '/projects/01-tinder-swipe/index.html'
-        }
-    },{
-        id: 2,
-        slug: '02-random-number-game',
-        title: 'Random Number Game',
-        description: 'Learn how to create a random number game with HTML, CSS and JavaScript',
-        learnings: [
-            'Dom',
-            'Events',
-            'Random numbers',
-            'Arrays'
-        ],
-        links: {
-            cover: '/projects/Project-02.webp',
-            github: 'https://github.com/agomzlo/js-projects/tree/main/02-random-number-game',
-            demo: '/projects/02-random-number-game/index.html'
-        }
-    }
+  {
+    id: 1,
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    description:
+      "Learn how to create a swipe card effect of tinder with HTML, CSS and JavaScript",
+    learnings: [
+      "Dom",
+      "Animations gestures",
+      "Mouse and finger touch events",
+      "CSS transitions",
+    ],
+    links: {
+      cover: "/projects/Project-01.webp",
+      github:
+        "https://github.com/agomzlo/js-projects/tree/main/01-tinder-swipe",
+      demo: "/projects/01-tinder-swipe/index.html",
+    },
+  },
+  {
+    id: 2,
+    slug: "02-random-number-game",
+    title: "Random Number Game",
+    description:
+      "Learn how to create a random number game with HTML, CSS and JavaScript",
+    learnings: ["Dom", "Events", "Random numbers", "Arrays"],
+    links: {
+      cover: "/projects/Project-02.webp",
+      github:
+        "https://github.com/agomzlo/js-projects/tree/main/02-random-number-game",
+      demo: "/projects/02-random-number-game/index.html",
+    },
+  },
+  {
+    id: 3,
+    slug: "03-three-in-a-row",
+    title: "Three in a row",
+    description:
+      "Learn how to create a three in a row game with HTML, CSS and JavaScript",
+    learnings: ["Dom", "Events", "Arrays", "Basic Algorithmic"],
+    links: {
+      cover: "/projects/Project-03.webp",
+      github:
+        "https://github.com/agomzlo/js-projects/tree/main/03-three-in-a-row",
+      demo: "/projects/03-three-in-a-row/index.html",
+    },
+  },
 ];
